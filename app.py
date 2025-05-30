e4e69cfb3cfbddeaa81637c13b6deebf27a379c8
rderakhshan <riemann.derakhshan@gmail.com>
2025-05-30 16:25:40 +0200

    modification no 006 adjusting output structure file

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate
+
 
 from src.utilities.utils import read_file_txt
 
@@ -10,10 +12,11 @@ from src.utilities.utils import read_file_txt
 load_dotenv()
 
 # Read the article content from the specified file
-article_source_path = "/Users/vahid/Documents/AI-Apps/Langchain-school/src/datapool/"  # Replace with your file path
+article_source_path = "./src/datapool/"  # Replace with your file path
 article_content     = "article.txt"
 article             = read_file_txt(article_source_path, article_content)
 article_title       = "Unlocking the Future: The Rise of Neuro-Symbolic AI Agents"
+
 
 # Initialize the OpenAI chat models with different configurations
 creative_llm = ChatOpenAI(
@@ -28,6 +31,17 @@ accurate_llm = ChatOpenAI(
     model="gpt-4o",  # Note: "gpt-4o-mini" is not a valid model name
     openai_api_key=os.getenv("OPENAI_API_KEY")
 )
+
+
+class Paragraph(BaseModel):
+    original_paragraph: str = Field(description="The original paragraph")
+    edited_paragraph: str = Field(description="The improved edited paragraph")
+    feedback: str = Field(description=(
+        "Constructive feedback on the original paragraph"
+    ))
+
+structured_llm = creative_llm.with_structured_output(Paragraph)
+
 
 # Defining system prompt templates
 system_prompt = SystemMessagePromptTemplate.from_template(
@@ -45,32 +59,29 @@ the article. The article is here for you to examine:
 
 ---
 
-and here is the articlw title:
-
----
-
-{article_title}
-
----
-
-Output the SEO friendly article description. Do not output
-anything other than the description.""",
-    input_variables = ["article", "article_title"]
+You should, first output the article title, then
+write a concise, SEO-friendly description that summarizes the article's content.
+It should be engaging and informative, ideally between 450-500 characters.""",
+    input_variables = ["article",]
 )
 
-first_prompt = ChatPromptTemplate.from_messages([system_prompt, user_prompt])
+prompt = ChatPromptTemplate.from_messages([system_prompt, user_prompt])
 # print(first_prompt.format(article = article))
 
+
+# chain : inputs: article / output: article_para
 chain = (
-    {"article": lambda x: x["article"],
-     "article_title": lambda x: x["article_title"]}
-    | first_prompt
-    | creative_llm
-    | {"article_title": lambda x: x.content}
+    {"article": lambda x: x["article"]}
+    | prompt
+    | structured_llm
+    | {
+        "original_paragraph": lambda x: x.original_paragraph,
+        "edited_paragraph": lambda x: x.edited_paragraph,
+        "feedback": lambda x: x.feedback
+    }
 )
 
-article_title_msg = chain.invoke({"article": article, 
-                                      "article_title": article_title
-                                      })
-print(f"Generated article title: {article_title_msg['article_title']}")
+result = chain.invoke({"article": article})
 
+print(result)
+ 
